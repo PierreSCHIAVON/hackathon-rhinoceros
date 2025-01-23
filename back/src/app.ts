7abe6dfdb03247bca9752fe74cab197e86921387
lucas <lucas.escaffre@ynov.com>
2025-01-23 11:48:38 +0100

    fix: lint config + merge conflict
    add: Installation methods in README.md

--- back/src/app.ts
+++ back/src/app.ts
@@ -8,21 +8,21 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log('user connected');
+  console.log('user connected');
 
-    socket.on('chat', (msg: string) => {
-        io.emit('chat', msg);
-    });
+  socket.on('chat', (msg: string) => {
+    io.emit('chat', msg);
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log('Server running on port 3000');
 });
