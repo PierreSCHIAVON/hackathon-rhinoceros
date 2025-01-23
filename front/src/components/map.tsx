3cf672d08a699fb1347f159e2c44b00263f5b183
LSCHOTT <lilian.schott@ynov.com>
2025-01-23 14:50:55 +0100

    map zones work

--- front/src/components/map.tsx
+++ front/src/components/map.tsx
@@ -3,32 +3,38 @@ import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import { LatLngTuple } from "leaflet";
 
 
-
 const MapComponent = () => {
     const [geojsonData, setGeojsonData] = useState(null);
+
     useEffect(() => {
-    fetch("/map.geojson")
-    .then((response) => response.json())
-    .then((data) => setGeojsonData(data));
-}, []);
+        fetch("/map.geojson")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => setGeojsonData(data))
+            .catch((error) => console.error("Error fetching GeoJSON data:", error));
+    }, []);
 
-const position : LatLngTuple = [43.6, 1.45];
-const geoJsonStyle = {
-    color: "blue",
-    weight: 2,
-    fillColor: "lightblue",
-    fillOpacity: 0.5,
-};
+    const position: LatLngTuple = [43.6, 1.45];
+    const geoJsonStyle = {
+        color: "blue",
+        weight: 2,
+        fillColor: "lightblue",
+        fillOpacity: 0.5,
+    };
 
-return (
-    <MapContainer center={position} zoom={13} style={{ height: "50vh", width: "75%",margin:"auto" }}>
-    <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    />
-    {geojsonData && <GeoJSON data={geojsonData} style={geoJsonStyle} />}
-    </MapContainer>
-);
+    return (
+        <MapContainer center={position} zoom={12} style={{ height: "75vh", width: "90%"}}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {geojsonData && <GeoJSON data={geojsonData} style={geoJsonStyle} />}
+        </MapContainer>
+    );
 };
 
 export default MapComponent;
