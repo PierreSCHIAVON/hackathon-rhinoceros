e2717388bd44218593c47381951bd24cc59b23f0
RedeiFantasmi <62249311+RedeiFantasmi@users.noreply.github.com>
2025-01-23 15:53:23 +0100

    feature(v0.5): First design

---
+++ front/src/components/activityCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import IActivity from '../interfaces/IActivity';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from '@material-tailwind/react';
+
+const ActivityCard: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  
+
+  return (
+    <Card className="w-full">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          { activity.title }
+        </Typography>
+        <Typography variant='small'>Lieu : { activity.zone.name }</Typography>
+        <Typography variant='paragraph'>{ activity.description }</Typography>
+      </CardBody>
+      {/* <CardFooter className="pt-0">
+        <Button>Read More</Button>
+      </CardFooter> */}
+    </Card>
+  );
+};
+
+export default ActivityCard;
