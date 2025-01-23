45543fe883711ee4efe7a68b304673c1df9b9cff
lucas <lucas.escaffre@ynov.com>
2025-01-23 15:10:13 +0100

    fix: comment one line to doesn't have error

--- back/src/routes/AlertRouter.ts
+++ back/src/routes/AlertRouter.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 const alertController = new AlertController(new AlertService());
 
 router.get('/alerts', alertController.getAlerts.bind(alertController));
-router.get('/alerts/:id', alertController.getAlert.bind(alertController));
+//router.get('/alerts/:id', alertController.getAlert.bind(alertController));
 //router.post('/alerts', alertController.createAlert.bind(alertController));
 //router.put('/alerts/:id', alertController.updateAlert.bind(alertController));
 //router.delete('/alerts/:id', alertController.deleteAlert.bind(alertController));
